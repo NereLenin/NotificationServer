a96693af6321358cf81c3831f9b5940265a19986
NereLenin <40742643+NereLenin@users.noreply.github.com>
2022-05-18 23:23:33 +0300

    Add files via upload

--- app/Views/auth/forgot_password.php
+++ app/Views/auth/forgot_password.php
@@ -1,15 +1,18 @@
-<h1><?php echo lang('Auth.forgot_password_heading');?></h1>
-<p><?php echo sprintf(lang('Auth.forgot_password_subheading'), $identity_label);?></p>
-
-<div id="infoMessage"><?php echo $message;?></div>
-
-<?php echo form_open('auth/forgot_password');?>
-
-      <p>
-      	<label for="identity"><?php echo (($type === 'email') ? sprintf(lang('Auth.forgot_password_email_label'), $identity_label) : sprintf(lang('Auth.forgot_password_identity_label'), $identity_label));?></label> <br />
-      	<?php echo form_input($identity);?>
-      </p>
-
-      <p><?php echo form_submit('submit', lang('Auth.forgot_password_submit_btn'));?></p>
-
-<?php echo form_close();?>
+<?= $this->extend('templates/layout');
+echo strlen($message) ?>
+<?= $this->section('content') ?>
+    <div class="container">
+        <div class="col-lg-6">
+            <h1><?php echo lang('Auth.forgot_password_heading'); ?></h1>
+            <p><?php echo sprintf(lang('Auth.forgot_password_subheading'), $identity_label); ?></p>
+            <div id="infoMessage"><?php echo $message; ?></div>
+            <?php echo form_open('auth/forgot_password'); ?>
+            <div class="form-group">
+                <label for="identity"><?php echo(($type === 'email') ? sprintf(lang('Auth.forgot_password_email_label'), $identity_label) : sprintf(lang('Auth.forgot_password_identity_label'), $identity_label)); ?></label>
+                <?php echo form_input($identity, '', 'class="form-control"'); ?>
+            </div>
+            <p><?php echo form_submit('submit', lang('Auth.forgot_password_submit_btn'), 'class="btn btn-primary"'); ?>
+                <?php echo form_close(); ?>
+        </div>
+    </div>
+<?= $this->endSection() ?>
--- app/Views/auth/register_user.php
+++ app/Views/auth/register_user.php
@@ -1,59 +1,59 @@
-<?= $this->extend('templates/layout'); echo strlen($message)?>
+<?= $this->extend('templates/layout');
+echo strlen($message) ?>
 <?= $this->section('content') ?>
-
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-6 col-12 ">
-            <h1><?php echo lang('Auth.login_heading');?></h1>
-            <p><?php echo lang('Auth.login_subheading');?></p>
+            <h1><?php echo lang('Auth.login_heading'); ?></h1>
+            <p><?php echo lang('Auth.login_subheading'); ?></p>
             <?php if (isset($message)): ?>
-                <div class="alert alert-danger">
-                <?php echo $message;?>
+                <div class="alert alert-danger mb-3">
+                    <?php echo $message; ?>
                 </div>
             <?php endif ?>
-            <?php echo form_open('auth/register_user');?>
-            <div class="mb-3">
-          <?php echo form_label(lang('Auth.create_user_fname_label'), 'first_name');?> <br />
-            <?php echo form_input($first_name);?>
+            <?php echo form_open('auth/register_user'); ?>
+            <div class="form-group">
+                <?php echo form_label(lang('Auth.create_user_fname_label'), 'first_name'); ?> <br/>
+                <?php echo form_input($first_name, '', 'class="form-control"'); ?>
             </div>
-            <div class="mb-3">
-            <?php echo form_label(lang('Auth.create_user_lname_label'), 'last_name');?> <br />
-            <?php echo form_input($last_name);?>
+            <div class="form-group">
+                <?php echo form_label(lang('Auth.create_user_lname_label'), 'last_name'); ?> <br/>
+                <?php echo form_input($last_name, '', 'class="form-control"'); ?>
             </div>
             <?php
             if ($identity_column !== 'email') {
-            echo '<div class="mb-3">';
-            echo form_label(lang('Auth.create_user_identity_label'), 'identity');
-            echo '<br/>';
-            echo \Config\Services::validation()->getError('identity');
-            echo form_input($identity);
-            echo '</div>';
+                echo '<div class="mb-3">';
+                echo form_label(lang('Auth.create_user_identity_label'), 'identity');
+                echo '<br/>';
+                echo \Config\Services::validation()->getError('identity');
+                echo form_input($identity);
+                echo '</div>';
             }
             ?>
-            <div class="mb-3">
-            <?php echo form_label(lang('Auth.create_user_company_label'), 'company');?> <br />
-            <?php echo form_input($company);?>
+            <div class="form-group">
+                <?php echo form_label(lang('Auth.create_user_company_label'), 'company'); ?> <br/>
+                <?php echo form_input($company, '', 'class="form-control"'); ?>
             </div>
-            <div class="mb-3">
-            <?php echo form_label(lang('Auth.create_user_email_label'), 'email');?> <br />
-            <?php echo form_input($email);?>
+            <div class="form-group">
+                <?php echo form_label(lang('Auth.create_user_email_label'), 'email'); ?> <br/>
+                <?php echo form_input($email, '', 'class="form-control"'); ?>
             </div>
-            <div class="mb-3">
-            <?php echo form_label(lang('Auth.create_user_phone_label'), 'phone');?> <br />
-            <?php echo form_input($phone);?>
+            <div class="form-group">
+                <?php echo form_label(lang('Auth.create_user_phone_label'), 'phone'); ?> <br/>
+                <?php echo form_input($phone, '', 'class="form-control"'); ?>
             </div>
-            <div class="mb-3">
-            <?php echo form_label(lang('Auth.create_user_password_label'), 'password');?> <br />
-            <?php echo form_input($password);?>
+            <div class="form-group">
+                <?php echo form_label(lang('Auth.create_user_password_label'), 'password'); ?> <br/>
+                <?php echo form_input($password, '', 'class="form-control"'); ?>
             </div>
-            <div class="mb-3">
-            <?php echo form_label(lang('Auth.create_user_password_confirm_label'), 'password_confirm');?> <br />
-            <?php echo form_input($password_confirm);?>
+            <div class="form-group">
+                <?php echo form_label(lang('Auth.create_user_password_confirm_label'), 'password_confirm'); ?> <br/>
+                <?php echo form_input($password_confirm, '', 'class="form-control"'); ?>
             </div>
-            <div class="mb-3">
-            <?php echo form_submit('submit', lang('Auth.create_user_submit_btn'));?>
+            <div>
+                <?php echo form_submit('submit', lang('Auth.create_user_submit_btn'), 'class="btn btn-primary"'); ?>
             </div>
-            <?php echo form_close();?>
+            <?php echo form_close(); ?>
         </div>
     </div>
 </div>
