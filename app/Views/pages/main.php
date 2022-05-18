aa91a28d8248bdc7a14a3a1210a3d1061d4e6532
NereLenin <40742643+NereLenin@users.noreply.github.com>
2022-05-18 23:19:03 +0300

    Add files via upload

--- app/Views/pages/main.php
+++ app/Views/pages/main.php
@@ -1,7 +1,17 @@
 <?= $this->extend('templates/layout') ?>
 <?= $this->section('content') ?>
-    <div class="jumbotron text-center">
-        <p class="lead">Веб интерфейс для менеджера уведомлений.</p>
-        <a class="btn btn-primary btn-lg" href="auth/login" role="button">Войти</a>
+<div class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 mb-3 md-sm-0">
+                <img src="/images/main.jpg" alt="">
+            </div>
+            <div class="col-sm-6 text-center text-sm-left d-flex flex-column justify-content-center">
+                <p class="lead">Веб интерфейс для менеджера уведомлений.</p>
+                <a class="btn btn-primary btn-lg" href="auth/login" role="button">Войти</a>
+            </div>
+        </div>
     </div>
+</div>
+
 <?= $this->endSection() ?>
